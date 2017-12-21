8b4ebbdf567ba2367320607701f6e82aa843bdd3
DeeDeeG <DeeDeeG@users.noreply.github.com>
2017-12-20 20:49:37 -0500

    Add tests for ARIA labels on search buttons (splash page)

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -7,3 +7,8 @@ When(/^I search from (.*?)$/) do |location|
   mock_location location
   find('.current-location-button').click
 end
+
+Then(/^the search buttons should have ARIA labels$/) do
+  expect(page).to have_button(class: 'submit-search-button', aria-label: => 'Search')
+  expect(page).to have_button(class: 'current-location-button', aria-label: => 'Search by Current Location')
+end
