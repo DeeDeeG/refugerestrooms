6563c045fd66eabf2cb22c200ceec18e71a1a805
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-10-05 18:24:25 -0400

    setup_vagrant.sh: cleanup top of script
    Rearranges package installations
    under their own headers.
    
    Much more readable this way.

--- setup/setup_vagrant.sh
+++ setup/setup_vagrant.sh
@@ -13,16 +13,24 @@ echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources
 # Add the apt-repository for the latest node.js
 curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
 
+# Install a bunch of required packages
+#(Who knows if these are actually needed??)
+
 sudo apt-get update -qq && sudo apt-get install -y build-essential libpq-dev nodejs
-sudo apt-get install build-essential chrpath libssl1.0-dev libxft-dev -y && \
-  sudo apt-get install libfreetype6 libfreetype6-dev -y && \
-  sudo apt-get install libfontconfig1 libfontconfig1-dev -y && \
-  export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64" && \
+sudo apt-get install -y chrpath libssl1.0-dev libxft-dev && \
+  sudo apt-get install -y libfreetype6 libfreetype6-dev && \
+  sudo apt-get install -y libfontconfig1 libfontconfig1-dev
+
+# Install Phantom JS
+export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64" && \
   wget https://github.com/Medium/phantomjs/releases/download/v2.1.1/$PHANTOM_JS.tar.bz2 && \
   tar xvjf $PHANTOM_JS.tar.bz2 && \
   sudo mv $PHANTOM_JS /usr/local/share && \
   sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin && \
-  sudo apt-get install -y yarn
+
+# Install Yarn
+echo 'installing yarn'
+sudo apt-get install -y yarn
 
 # required packages
 declare -A packages
